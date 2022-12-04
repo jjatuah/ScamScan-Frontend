713110b7c71d95a6048839b55fe27bb778e3a042
unknown <jjatuah@gmail.com>
2022-12-04 11:38:12 +0100

    started working on the scan form

---
+++ scamscan-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,65 @@
+import { useRef } from "react";
+import "./Home.css";
+import Navbar from "../../components/navbar/Navbar";
+import clockImg from "../../assets/clock.png";
+import share from "../../assets/share.png";
+import upload from "../../assets/upload.png";
+import { AiOutlineClockCircle } from 'react-icons/ai';
+
+
+const Home = () => {
+
+  const hiddenFileInput = useRef(null);
+
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  }
+
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+  }
+
+
+  return ( 
+    <div className="home">
+      <Navbar />
+
+      <div className="inputSection">
+        <form action="">
+          <div className="inputHeaders">
+            <div className="depth">
+              <label>Depth:</label> <span>Basic Scan</span> <span>Advanced Scan</span> 
+            </div>
+            <p>Result</p>
+            <p className="clock"> <AiOutlineClockCircle style={{color:"#333333", fontSize: "22px"}}/> </p>
+          </div>
+
+          <div className="lowerSection">
+            <div className="inputs">
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+              <div className="uploadScan">
+                <div className="upload">
+                  <img src={upload} alt="#" />
+                  <input type="file" name="file" id="file" ref={hiddenFileInput} onChange={handleChange} style={{display:'none'}} />
+                  <label onClick={handleClick}>Upload File</label>
+                </div>
+              </div>
+            </div>
+
+            <div className="result">
+              <div className="blank">
+                <div className="blankResult"></div>
+                <div className="resultShare">
+                  <img src={share} alt="" />
+                  <p>Share Result</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+   );
+}
+ 
+export default Home;
