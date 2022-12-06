5eaeb2dfdecf08427a7efccd85ce5ffabf05d2a2
unknown <jjatuah@gmail.com>
2022-12-06 20:22:01 +0100

    started work on the team page

---
+++ scamscan-frontend/src/pages/team/Team.jsx
@@ -0,0 +1,16 @@
+import "./Team.css";
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+
+
+const Team = () => {
+  return ( 
+    <div className="team">
+      <Navbar />
+
+      <Footer />
+    </div>
+   );
+}
+ 
+export default Team;
