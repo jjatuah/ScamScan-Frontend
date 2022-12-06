b772aeade44634cc7e77782b7d53ab1da0e40aa6
unknown <jjatuah@gmail.com>
2022-12-06 09:02:45 +0100

    100% done with html code for the footer

--- scamscan-frontend/src/components/footer/Footer.jsx
+++ scamscan-frontend/src/components/footer/Footer.jsx
@@ -1,4 +1,11 @@
+
 import "./Footer.css";
+import facebook from "../../assets/facebook.png";
+import twitter from "../../assets/twitter.png";
+import linkdIn from "../../assets/linkdIn.png";
+import android from "../../assets/android.png";
+import apple from "../../assets/apple.png";
+
 
 
 const Footer = () => {
@@ -8,50 +15,56 @@ const Footer = () => {
 
         <div className="footerLink">
           <h3>Product</h3>
-          <a href="#"></a>
-          <a href="#"></a>
-          <a href="#"></a>
-          <a href="#"></a>
-          <a href="#"></a>
+          <a href="#">Individuals</a>
+          <a href="#">Team</a>
+          <a href="#">Technology</a>
+          <a href="#">Security</a>
+          <a href="#">Integrations</a>
         </div>
 
         <div className="footerLink">
-          <h3>Product</h3>
-          <a href="#"></a>
-          <a href="#"></a>
-          <a href="#"></a>
+          <h3>Resources</h3>
+          <a href="#">How-To's</a>
+          <a href="#">Case Studies</a>
+          <a href="#">Help</a>
         </div>
 
         <div className="footerLink">
-          <h3>Product</h3>
-          <p></p>
-          <p></p>
-          <p></p>
+          <h3>Headquarters</h3> 
+          <p>GDG Lafia, 02 Innov</p>
+          <p>Lab, Lafia, Nasarawa</p>
+          <p>State</p>
         </div>
 
         <div className="footerLink">
-          <h3>Product</h3>
+          <h3>Connect</h3>
           <div className="socials">
-            <a href="#">{}</a>
-            <a href="#">{}</a>
-            <a href="#">{}</a>
+            <a href="#"><img src={twitter} alt="twitter logo" /></a>
+            <a href="#"><img src={facebook} alt="facebook logo" /></a>
+            <a href="#"><img src={linkdIn} alt="linkdin logo"/></a>
           </div>
         </div>
 
         <div className="footerLink">
-          <h3></h3>
+          <h3>Apps</h3>
           <div className="appLink">
-            <a href="#"><img src="" alt="#" /></a>
-            <a href="#"><img src="" alt="#" /></a>
+            <a href="#"><img src={apple} alt="#" /></a>
+            <a href="#"><img src={android} alt="#" /></a>
           </div>
         </div>
 
       </div>
 
       <div className="extraInfo">
-        <div className="copyright"></div>
+        <div className="copyright">
+          <p>Copyright 2022 (c) GDG Lafia</p>
+        </div>
 
-        <div className="extraLinks"></div>
+        <div className="extraLinks">
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms of Use</a>
+          <a href="#">DMCA and Copyright Abuse Policy</a>
+        </div>
       </div>
     </div>
    );
